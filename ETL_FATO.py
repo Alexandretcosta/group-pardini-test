cca33ca7910e46fa80a5c1d8c36e91ede3f0f2f0
Alexandre Teixeira Costa <37586673+Alexandretcosta@users.noreply.github.com>
2023-03-10 02:10:44 -0300

    Add ETL_FATO.py

---
+++ ETL_FATO.py
@@ -0,0 +1,133 @@
+import pyodbc
+import pandas as pd
+from dotenv import load_dotenv
+import os
+from json import load
+
+load_dotenv("keys.env",override=True)
+
+driver = os.environ['DRIVER']
+server = os.environ['SERVER']
+database = os.environ['DATABASE']
+
+### funcoes 
+
+def conexao(driver, server, database):
+    
+    cnxn_str = (f"Driver={str(driver)};"
+                f"Server={str(server)};"
+                f"Database={str(database)};"
+                "Trusted_Connection=yes;")
+    cnxn = pyodbc.connect(cnxn_str)
+    return cnxn
+
+
+def truncate_table(driver, server, database, tabela_destino):
+    
+    con = conexao(driver, server, database)
+    
+    truncate = f"""TRUNCATE TABLE {tabela_destino} """
+    
+    cursor = con.cursor()
+    cursor.execute(truncate)
+    
+    con.commit()
+    cursor.close()
+    
+    return
+
+print('Driver: ',str(driver))
+print('Server: ',str(server))
+print('Database: ',str(database))
+
+con = conexao(driver, server, database)
+print("Conexao Feita com Sucesso")
+
+## Tabela Fato
+
+filename = 'fato/Fato_Grao.ipynb'
+with open(filename) as fp:
+    nb = load(fp)
+
+for cell in nb['cells']:
+    if cell['cell_type'] == 'code':
+        source = ''.join(line for line in cell['source'] if not line.startswith('%'))
+        exec(source, globals(), locals())
+        
+
+## Tabela Dimensao Unidade       
+filename = 'dimensao/Unidade.ipynb'
+with open(filename) as fp:
+    nb = load(fp)
+
+for cell in nb['cells']:
+    if cell['cell_type'] == 'code':
+        source = ''.join(line for line in cell['source'] if not line.startswith('%'))
+        exec(source, globals(), locals())
+
+## Tabela Dimensao Exame
+filename = 'dimensao/Exame.ipynb'
+with open(filename) as fp:
+    nb = load(fp)
+
+for cell in nb['cells']:
+    if cell['cell_type'] == 'code':
+        source = ''.join(line for line in cell['source'] if not line.startswith('%'))
+        exec(source, globals(), locals())
+
+        
+## Tabela Dimensao Convenio
+filename = 'dimensao/Convenio.ipynb'
+with open(filename) as fp:
+    nb = load(fp)
+
+for cell in nb['cells']:
+    if cell['cell_type'] == 'code':
+        source = ''.join(line for line in cell['source'] if not line.startswith('%'))
+        exec(source, globals(), locals())
+
+        
+## Tabela Dimensao Unidade Produtiva
+filename = 'dimensao/Unidade_Produtiva.ipynb'
+with open(filename) as fp:
+    nb = load(fp)
+
+for cell in nb['cells']:
+    if cell['cell_type'] == 'code':
+        source = ''.join(line for line in cell['source'] if not line.startswith('%'))
+        exec(source, globals(), locals())
+
+
+## Tabela Dimensao Unidade Negocio
+filename = 'dimensao/Unidade_Negocio.ipynb'
+with open(filename) as fp:
+    nb = load(fp)
+
+for cell in nb['cells']:
+    if cell['cell_type'] == 'code':
+        source = ''.join(line for line in cell['source'] if not line.startswith('%'))
+        exec(source, globals(), locals())
+        
+## Tabela Dimensao Pedido
+filename = 'dimensao/Pedido.ipynb'
+with open(filename) as fp:
+    nb = load(fp)
+
+for cell in nb['cells']:
+    if cell['cell_type'] == 'code':
+        source = ''.join(line for line in cell['source'] if not line.startswith('%'))
+        exec(source, globals(), locals())
+
+#!runipy fato/Fato_Grao.ipynb
+
+#!runipy dimensao/Unidade.ipynb
+
+#!runipy dimensao/Exame.ipynb
+
+#!runipy dimensao/Convenio.ipynb
+
+#!runipy dimensao/Unidade_Produtiva.ipynb
+
+#!runipy dimensao/Unidade_Negocio.ipynb
+
+#!runipy dimensao/Pedido.ipynb
